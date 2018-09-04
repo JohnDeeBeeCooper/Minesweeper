dd57266dbfdbeafcde2279803a507c9768bc7b43
JohnDeeBeeCooper <vladislav8519@gmail.com>
2018-09-04 23:58:23 +0300

    added border

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,28 +4,42 @@ import Cell from './Cell';
 
 const Field = (props) => {
     return (
-        <Container>
-            <Table gameOver={props.theEnd}>
-                <Tbody>
-                    {props.array.map(item => <Row>{item.map(a => <Cell flagOn={props.flagOn} func={props.handleChange} key={a.id} param={a} />)}</Row>)}
-                </Tbody>
-            </Table>
-        </Container>
+        <Wrapper>
+            <Container>
+                <Table gameOver={props.theEnd}>
+                    <Tbody>
+                        {props.array.map(item => <Row>{item.map(a => <Cell flagOn={props.flagOn} func={props.handleChange} key={a.id} param={a} />)}</Row>)}
+                    </Tbody>
+                </Table>
+            </Container>
+        </Wrapper>
     )
 }
 const Container = styled.div`
+    background: #bdbdbd;
+    padding: 7px;
+    border-bottom: solid 2px #707070;
+    border-right: solid 2px #707070;
+    border-top: solid 2px #fff;
+    border-left: solid 2px #fff;
+`;
+const Wrapper = styled.div`
     margin-top: 50px;
     display: flex;
     justify-content: center;
 `;
 const Table = styled.table`
+    border-bottom: solid 2px #fff;
+    border-right: solid 2px #fff;
+    border-top: solid 2px #7b7b7b;
+    border-left: solid 2px #7b7b7b;
+    border-spacing: 0px;
     ${props => props.gameOver && css`
     pointer-events: none;
     cursor: default;`}
-    border-spacing: 0px;
 `;
 const Row = styled.tr`
-    `;
+`;
 const Tbody = styled.tbody`
     display: table-row-group;
     vertical-align: middle;
